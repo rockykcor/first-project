57ec1c8e0318dc1b1b620feefda0bd4faededd13
Rocky <rockyzhen@gmail.com>
2023-05-25 15:28:02 +0800

    no message

---
+++ src/main/java/TestServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Test")
+public class TestServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		pw.println("HI We go");
+	}
+}
